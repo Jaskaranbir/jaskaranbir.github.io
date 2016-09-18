eade69b8b19f72373f3f153d911e0f53ac55d6e5
Jaskaranbir <jaskaranbir_singh@ymail.com>
2016-09-18 15:54:09 -0400

    Fix inaccurate contact mail status - 3

--- script.js
+++ script.js
@@ -452,7 +452,7 @@ function setContact() {
             success: function (data) {
                 $('#contact-send').addClass('contact-button-finalize');
                 mailSuccess = true;
-                $contactStatus.text('An Error Occurred. Please make sure the email you entered is valid. You may also want to contact me directly on my email: dhjaskar@sheridancollege.ca');
+                $contactStatus.text('Mail Sent Successfully');
                 return;
             }
         });
